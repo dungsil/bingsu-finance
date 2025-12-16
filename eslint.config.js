71a72ef123cd284eaa89776791df8624941487bd
김용건 (Kim Younggeon) <git+commit@younggeon.kim>
2025-12-16 21:46:20 +0900

    chore: eslint 설정 수정

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,15 @@
 // @ts-check
 import antfu from '@antfu/eslint-config'
 
-export default antfu({
-  vue: true,
-})
+export default antfu(
+  {
+    vue: true,
+  },
+  [
+    {
+      rules: {
+        'test/prefer-lowercase-title': 'off',
+      },
+    },
+  ],
+)
